ff128b50c4e4e5bf510a64837e3d4f1ac4ac800e
Cameron Steele <cam.steeleis@gmail.com>
2018-10-10 14:26:29 -0500

    Fixed Turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Turret : MonoBehaviour {
+    public Transform playerTarget;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Debug.Log("Looking for Camera");
+        this.transform.LookAt(playerTarget);
+	}
+}
