10dc0a9fbe59b43db36a5de25c153148b86efbff
Cameron Steele <cam_steele@ymail.com>
2018-10-10 12:01:58 -0500

    Inital Commit

---
+++ Assets/Scenes/ProjectileScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+    public Transform projectileTarget;
+    public Rigidbody projectileRigidBody;
+
+    public float turn;
+    public float projectileVelocity;
+
+    private void FixedUpdate()
+    {
+        projectileRigidBody.velocity = transform.forward * projectileVelocity;
+
+        var projectileTargetRotation = Quaternion.LookRotation(projectileTarget.position - transform.position);
+
+        projectileRigidBody.MoveRotation(Quaternion.RotateTowards(transform.rotation, projectileTargetRotation, turn));
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
